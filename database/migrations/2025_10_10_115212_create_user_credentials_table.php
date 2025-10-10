6dc38f0b9b28cb99fb6a58af13f9257b800cc9d8
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-10-10 16:14:06 +0300

    change

--- database/migrations/2025_10_10_115212_create_user_credentials_table.php
+++ database/migrations/2025_10_10_115212_create_user_credentials_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('user_credentials', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('provider_name');
-            $table->string('provider_id')->unique();
-            $table->string('provider_token')->nullable();
-            $table->string('provider_refresh_token')->nullable();
+            $table->text('provider_name');
+            $table->text('provider_id')->unique();
+            $table->text('provider_token')->nullable();
+            $table->text('provider_refresh_token')->nullable();
             $table->timestamps();
         });
     }
