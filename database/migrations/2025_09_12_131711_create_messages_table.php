b39a322352930c6439e641a4b2fbc4293b389ca0
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-10-18 16:05:38 +0300

    message id

--- database/migrations/2025_09_12_131711_create_messages_table.php
+++ database/migrations/2025_09_12_131711_create_messages_table.php
@@ -14,12 +14,13 @@ return new class extends Migration {
       $table->id();
       $table->foreignId('user_id')->constrained()->cascadeOnDelete();
 
+      $table->unsignedBigInteger('message_id')->nullable();
       $table->string('from_email')->nullable()->index();
       $table->string('from_name')->nullable()->index();
       $table->string('to_email')->nullable()->index();
       $table->string('to_name')->nullable()->index();
 
-      $table->string('subject')->index();
+      $table->text('subject')->index();
       $table->longText('body_text')->nullable();
       $table->string('source')->nullable()->index();
 
