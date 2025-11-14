24bddb0b2b3237c7e3c9ee643ad51e1819f70ec2
ibrahim elshorbagy <e.mohmed55@gmail.com>
2025-11-14 19:35:01 +0200

    rebuild

--- resources/js/Pages/Admin/SubscriptionSystem/Plans/Partials/Pages/EditPlan.jsx
+++ resources/js/Pages/Admin/SubscriptionSystem/Plans/Partials/Pages/EditPlan.jsx
@@ -186,7 +186,9 @@ export default function EditPlan({ plan, features }) {
                         <h3 className="text-lg font-semibold flex items-center">
                           {section.title}
                         </h3>
-                        <div className="flex items-center gap-3">
+                        {/* System DOES NOT DESIGN TO HANDEL DISABLED FEATURES */}
+                        
+                        {/* <div className="flex items-center gap-3">
                           <span className="text-sm text-neutral-600 dark:text-neutral-400">
                             {isSectionActive(section.key) ? t('active') : t('inactive')}
                           </span>
@@ -199,7 +201,7 @@ export default function EditPlan({ plan, features }) {
                             />
                             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                           </label>
-                        </div>
+                        </div> */}
                       </div>
 
                       <div className="overflow-x-auto">
